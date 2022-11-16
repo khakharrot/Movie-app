73e599a67d9795a8fa1058f5b3b129e41ddf5942
khakharrot <kharrat.amine1@gmail.com>
2022-11-16 21:33:46 +0100

    fixed research

--- src/Components/Movielist.js
+++ src/Components/Movielist.js
@@ -5,7 +5,7 @@ import Moviecard from './Moviecard'
 function Movielist({Movies,SearchWord,SearchRating}) {
   return (
     <div className='list'>
-        {Movies.filter( (el)=> el.title.toLowerCase().includes(SearchWord.toLowerCase().trim() && el.rating >= SearchRating)
+        {(Movies.filter( (el)=> el.title.toLowerCase().includes(SearchWord.toLowerCase().trim()) && el.rating >= SearchRating)
         ).map((el)=>( <Moviecard el={el}/>))}
     </div>
   )
